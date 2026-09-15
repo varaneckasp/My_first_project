1b5f07a86521884c68222b766e4d2bad94e3cf68
varaneckasp <varaneckasp@gmail.com>
2026-09-15 18:24:37 +0300

    Struktura

---
+++ v0.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+using std::string;
+using std::vector;
+
+
+struct studentas{
+    string vardas, pavarde;
+    vector<int> pazymiai;
+    int exam;
+    };
+int main() {
+
+}
+ 
