6a1b6f0025d5e2e0e6e2c9b094ff45376fb51d27
varaneckasp <varaneckasp@gmail.com>
2026-09-16 18:38:37 +0300

    Atsitiktiniu rezultatu ivestis

--- v0.1.cpp
+++ v0.1.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <cstdlib>
+#include <ctime>
 using std::string;
 using std::vector;
 
@@ -16,6 +18,7 @@ struct studentas{
     void printas(studentas A, int isvedimo_tipas);
 int main() 
 {
+    srand(time(0));
     vector<studentas> grupe;
     std::cout << "Iveskite studentu skaiciu: ";
     int n;
@@ -25,6 +28,17 @@ int main()
     for(int i = 0; i < n; i++) {
         std::cout << "Iveskite varda ir pavarde per tarpa: ";
         std::cin >> A.vardas >> A.pavarde;
+        std::cout << "Ar pazymius ivesite ranka (1) ar generuosite atsitiktinius pazymius (2)? ";
+        int pasirinkimas;
+        std::cin >> pasirinkimas;
+        if (pasirinkimas == 2) {
+            int kiek_pazymiu = rand() % 10 + 1;
+            A.pazymiai.clear();
+            for (int j = 0; j < kiek_pazymiu; j++) {
+                int pazimys = rand() % 10 + 1;
+                A.pazymiai.push_back(pazimys);
+            }
+        } else {
         std::cout << "Iveskite studento namu darbu pazymius po viena spausdami enter, kai baigsite paspauskite enter nieko neivedus: ";
         std::cin.ignore();
     string ivestis;
@@ -45,8 +59,17 @@ int main()
             std::cout << "Ivestas netinkamas pazymys. Iveskite pazymi nuo 1 iki 10: ";
         }
     }
-    std::cout << "Iveskite studento egzamino rezultata: ";
-    std::cin >> A.exam;
+    }
+    std::cout <<"Ar norite ivesti ranka (1) ar atsitiktini egzamino rezultata (2)? ";
+    int pasirinkimas_egz;
+    std::cin >> pasirinkimas_egz;
+    if (pasirinkimas_egz == 2) {
+        A.exam = rand() % 10 + 1;
+    } else {
+        std::cout << "Iveskite studento egzamino rezultata: ";
+        std::cin >> A.exam;
+    }
+    
     double suma = 0;
     for (int i = 0; i < A.pazymiai.size(); i++) {
         suma += A.pazymiai[i];
@@ -68,7 +91,7 @@ int main()
     std::cout << "Kokio isvedimo tipa norite matyti? (1 - vidurkis, 2 - mediana, 3 - abu): ";
     int isvedimo_tipas;
     std::cin >> isvedimo_tipas;
-    std::cout << std::left << std::setw(15) << "Vardas " << std::setw(15) << "Pavarde " << std::setw(10) << "Galutinis (vidurkis) " << std::setw(10) << "Galutinis (mediana)" << std::endl;
+    std::cout << std::left << std::setw(15) << "Vardas " << std::setw(15) << "Pavarde " << std::setw(10) << "Galutinis(Vid.) " << std::setw(10) << "Galutinis(Med.)" << std::endl;
     std::cout << "---------------------------------------------------------------------\n";
     for (studentas B: grupe) {
         printas(B, isvedimo_tipas);
@@ -79,10 +102,10 @@ void printas(studentas A, int isvedimo_tipas) {
     std::cout << std::fixed << std::setprecision(2);
     std::cout << std::left << std::setw(15) << A.vardas << std::setw(15) << A.pavarde;
     if (isvedimo_tipas == 1 || isvedimo_tipas == 3) {
-        std::cout << std::setw(10) << A.galutinis;
+        std::cout <<std::right << std::setw(10) << A.galutinis;
     }
     if (isvedimo_tipas == 2 || isvedimo_tipas == 3) {
-        std::cout << std::setw(10) << A.galutinis_mediana;
+        std::cout <<std::right << std::setw(10) << A.galutinis_mediana;
     }
     std::cout << std::endl; 
 
