9a8c040e2ee121af6ad631bd16b794b7ace50cc5
varaneckasp <varaneckasp@gmail.com>
2026-09-16 15:42:05 +0300

    skaiciavimas su mediana

--- v0.1.cpp
+++ v0.1.cpp
@@ -16,8 +16,8 @@ int main()
     studentas A;
     std::cout << "Iveskite varda ir pavarde per tarpa: ";
     std::cin >> A.vardas >> A.pavarde;
-    std::cout << "Kiek namu darbu pazymiu norite ivesti? ";
     int k;
+    std::cout << "Iveskite pazymiu skaiciu: ";
     std::cin >> k;
     for (int i = 0; i < k; i++) {
         int pazymys;
@@ -25,6 +25,7 @@ int main()
         std::cin >> pazymys;
         A.pazymiai.push_back(pazymys);
     }
+    
     std::cout << "Iveskite studento egzamino rezultata: ";
     std::cin >> A.exam;
     double suma = 0;
@@ -32,8 +33,18 @@ int main()
         suma += A.pazymiai[i];
     }
     double vidurkis = suma / A.pazymiai.size();
+     double mediana;
+    if (A.pazymiai.size() % 2 == 0) {
+        int mid = A.pazymiai.size() / 2;
+        mediana = (A.pazymiai[mid - 1] + A.pazymiai[mid]) / 2.0;
+    } else {
+        int mid = A.pazymiai.size() / 2;
+        mediana = A.pazymiai[mid];
+    }
     double galutinis = 0.4 * vidurkis + 0.6 * A.exam;
+    double galutinis_mediana = 0.4 * mediana + 0.6 * A.exam;
     std::cout << "Studento galutinis rezultatas: " << std::fixed << std::setprecision(2) << galutinis << std::endl;
+    std::cout << "Studento galutinis rezultatas (naudojant mediana): " << std::fixed << std::setprecision(2) << galutinis_mediana << std::endl;
 
 }
  
